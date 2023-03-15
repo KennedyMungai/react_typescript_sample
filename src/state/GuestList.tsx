74cfce2e793b7221eece70175016da06310c1b53
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 11:18:27 +0300

    Wired up the input that to use the name state

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -8,7 +8,11 @@ const GuestList: React.FC = () =>
     return (
         <div>
             <h1>Guest List</h1>
-            <input type="text" />
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
             <button>Add Guest</button>
         </div>
     )
