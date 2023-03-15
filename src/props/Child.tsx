676a6da3609669745537d1f4625ba40958a46ba6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 10:28:47 +0300

    Fixed a small mistake in the markup

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -9,7 +9,7 @@ const Child = ({ color }: ChildProps) =>
 {
     return (
         <div>
-            <h1>`I am the child component. I am a {color} colored individual`</h1>
+            <h1>I am the child component. I am a {color} colored individual</h1>
         </div>
     )
 }
