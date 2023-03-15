be197b4d1abb35abe8449219ab1c253d8e9a2c96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 11:35:09 +0300

    Added some markup to the react component

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -4,7 +4,15 @@ import React, { useState } from 'react'
 const UserSearch = () =>
 {
     return (
-        <div>UserSearch</div>
+        <div>
+            <h1>Find User</h1>
+            <button>Find</button>
+            <div className="">
+                <h3>User Details</h3>
+                <p>Name: </p>
+                <p>Age: </p>
+            </div>
+        </div>
     )
 }
 
