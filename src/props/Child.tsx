971f0d3645b3a33d4acbae10a79d967e1fb40623
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 11:07:14 +0300

    Added color to the markup

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -14,6 +14,7 @@ const Child: FC<ChildProps> = ({ color, onClick, children }) =>
         <>
             {children}
             <button onClick={onClick}> Click Me</button>
+            {color}
         </>
     )
 }
