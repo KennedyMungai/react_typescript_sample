1101b682e9ed7667818a46be725faa6ceb69d380
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 11:15:59 +0300

    Added some simple markup for the Guest List app

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,7 +1,11 @@
 const GuestList: React.FC = () =>
 {
     return (
-        <div>GuestList</div>
+        <div>
+            <h1>Guest List</h1>
+            <input type="text" />
+            <button>Add Guest</button>
+        </div>
     )
 }
 
